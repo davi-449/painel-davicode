283a5118b5840598ff743e90dc6c1681aaa64512
DaviCode <silveira.odavid@gmail.com>
2026-04-06 16:08:56 -0300

    fix(auth): remove min password length restriction

--- backend/src/validators/authSchema.ts
+++ backend/src/validators/authSchema.ts
@@ -3,7 +3,7 @@ import { z, TypeOf } from 'zod';
 export const loginSchema = z.object({
   body: z.object({
     email: z.string().min(1, 'E-mail é obrigatório').email('E-mail inválido'),
-    senha: z.string().min(6, 'Senha deve ter no mínimo 6 caracteres'),
+    senha: z.string().min(1, 'Senha é obrigatória'),
   }),
 });
 
